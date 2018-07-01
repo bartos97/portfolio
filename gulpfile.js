b1e92c5aecf812cc8f5febdd16f2abb993becc00
bartos97 <bartoss97@gmail.com>
2018-07-01 13:07:36 +0200

    Init commit

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
+var wait = require('gulp-wait');
+
+
+// SASS task
+// compiles sass into css
+gulp.task('sass', function() {
+  return gulp.src('scss/main.scss')
+  .pipe(wait(100)) // becouse VS Code was making some problems with compiling on save
+  .pipe(sourcemaps.init())
+  .pipe(sass({
+    outputStyle: 'compressed'
+  }).on('error', sass.logError))
+  .pipe(sourcemaps.write())
+  .pipe(gulp.dest('css/'))
+  .pipe(browserSync.reload({
+    stream: true
+  })) 
+});
+
+
+// Bootstrap task
+// compiles bootstrap
+// gulp.task('bootstrap', function() {
+//   return gulp.src('scss/bootstrap/bootstrap.scss')
+//   .pipe(sourcemaps.init())
+//   .pipe(sass({
+//     outputStyle: 'compressed'
+//   }).on('error', sass.logError))
+//   .pipe(sourcemaps.write())
+//   .pipe(gulp.dest('css'))
+//   .pipe(browserSync.reload({
+//     stream: true
+//   })) 
+// });
+
+
+// prefixCss task
+gulp.task('prefixCss', function() {
+  return gulp.src('css/main.css')
+  .pipe(autoprefixer({
+    browsers: [ "last 3 versions", "iOS > 7", "Safari > 5", "Explorer >= 11" ]
+  }))
+  .pipe(gulp.dest('css'));
+});
+
+
+// browserSync task
+gulp.task('browserSync', function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: './'
+    }
+  });
+});
+
+
+// Watch task
+gulp.task('watch', function (){
+  gulp.watch(['*.html', 'js/*.js'], browserSync.reload);
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+
+// Default task
+gulp.task('default', function (callback) {
+  runSequence(['browserSync', 'watch'],
+    callback
+  );
+});
